e922a96375d7eafb5d95aac744857919d16872df
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:52:25 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/ExamPreparationFinal2/01.PasswordReset/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password  = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+
+                string[] commandArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string comamndType = commandArgs[0];
+
+                if (comamndType == "TakeOdd")
+                {
+                    char[] oddChars = password.
+                        Where((symbol, index) => index % 2 != 0 )
+                        .ToArray();
+
+                    password = string.Join("", oddChars);
+                    Console.WriteLine($"{password}");
+                }
+                else if (comamndType == "Cut")
+                {
+                    int indexForRemoving = int.Parse(commandArgs[1]);
+                    int lengthForRemoving = int.Parse(commandArgs[2]);
+
+                    password = password.Remove(indexForRemoving, lengthForRemoving);
+                    Console.WriteLine($"{password}");
+                }
+                else if (comamndType == "Substitute")
+                {
+                    string substring = commandArgs[1];
+                    string substitute = commandArgs[2];
+
+                    if (password.Contains(substring))
+                    {
+                        password = password.Replace(substring, substitute);
+                        Console.WriteLine($"{password}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Nothing to replace!");
+                    }
+                }
+
+                command = Console.ReadLine(); 
+            }
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ C# Programming Fundamentals/ExamPreparationFinal2/02.MirrorWords/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MirrorWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"((?<firstWord>#[A-Za-z]{3,}#)(?<secondWord>#[A-Za-z]{3,}#))|((?<firstWord>@[A-Za-z]{3,}@)(?<secondWord>@[A-Za-z]{3,}@))");
+            //Regex regex = new Regex(@"(@|#)(?<firstWord>[a-zA-Z]{3,})(\1)(\1)(?<secondWord>[a-zA-Z]{3,})(\1)");
+            List<string[]> mirrorPairs = new List<string[]>();
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                string wordOne = match.Groups["firstWord"].Value;
+                wordOne = wordOne.Trim(new Char[] { '#', '@' });
+                string wordTwoReversed = string.Join("", match.Groups["secondWord"].Value.Trim(new Char[] { '#', '@' }).Reverse());
+
+                if (wordOne == wordTwoReversed)
+                {
+                    mirrorPairs.Add(new string[2] { match.Groups["firstWord"].Value.Trim(new Char[] { '#', '@' }), match.Groups["secondWord"].Value.Trim(new Char[] { '#', '@' }) });
+                }
+            }
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+            }
+
+            if (mirrorPairs.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", mirrorPairs.Select(x => $"{x[0]} <=> {x[1]}")));
+            }
+        }
+    }
+}
