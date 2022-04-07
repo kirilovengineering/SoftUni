1bcfb6f8435a748707da33d500d51caa90fcc779
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:20:53 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(wordToRemove))
+            {
+                //int startIndexOfWordToRemove = text.IndexOf(wordToRemove);
+                //text = text.Remove(startIndexOfWordToRemove, wordToRemove.Length);
+
+                text = text.Replace(wordToRemove, "");
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ C# Programming Fundamentals/TextProcessing-Lab/05.DigitsLettersАndOther/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.DigitsLettersАndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringBuilder letters = new StringBuilder();
+            //StringBuilder numbers = new StringBuilder();
+            //StringBuilder otherChars = new StringBuilder();
+
+            string text = Console.ReadLine();
+
+            char[] allDigits = text
+                .Where(item => char.IsDigit(item))
+                .ToArray();
+
+            char[] allLetters = text
+                .Where((item) => char.IsLetter(item))
+                .ToArray();
+
+            char[] otherChars = text
+                .Where((item) => !char.IsLetterOrDigit(item))
+                .ToArray();
+
+            Console.WriteLine(string.Join("", allDigits));
+            // Console.WriteLine(allDigits.ToString()); това не работи
+            Console.WriteLine(allLetters);
+            Console.WriteLine(otherChars);
+
+            //foreach (char item in text)
+            //{
+            //    if (char.IsLetter(item))
+            //    {
+            //        letters.Append(item);
+            //    }
+            //    else if (char.IsDigit(item))
+            //    {
+            //        numbers.Append(item);
+            //    }
+            //    else
+            //    {
+            //        otherChars.Append(item);
+            //    }
+            //}
+
+            //Console.WriteLine(numbers.ToString());
+            //Console.WriteLine(letters.ToString());
+            //Console.WriteLine(otherChars.ToString());
+        }
+    }
+}
