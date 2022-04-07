3360801c832d07a0f8669151f6c5d3315aeb9e4f
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:53:56 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/ExamPreparationFinal1/01.SecretChat/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SecretChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string command = Console.ReadLine();
+
+            while (command != "Reveal")
+            {
+
+                string[] commandArgs = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+                string commandTyp = commandArgs[0];
+
+                if (commandTyp == "InsertSpace")
+                {
+                    int insertIndex = int.Parse(commandArgs[1]);
+                    message = message.Insert(insertIndex, " ");
+                }
+                else if (commandTyp == "Reverse")
+                {
+                    string substring = commandArgs[1];
+
+                    if (message.Contains(substring))
+                    {
+                        int substringIndex = message.IndexOf(substring);
+                        message = message.Remove(substringIndex, substring.Length);
+                        message = message + string.Join("", substring.Reverse());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"error");
+                        command = Console.ReadLine();
+                        continue;
+
+                    }
+                }
+                else if (commandTyp == "ChangeAll")
+                {
+                    string substring = commandArgs[1];
+                    string replacementtext = commandArgs[2];
+
+                    message = message.Replace(substring, replacementtext);
+                }
+
+                Console.WriteLine(message);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+
+        }
+    }
+}
---
+++ C# Programming Fundamentals/ExamPreparationFinal1/02.FancyBarcodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex patttern = new Regex(@"@#+[A-Z][A-Za-z\d]{4,}[A-Z]@#+");
+            int barcodeCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < barcodeCount; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                if (patttern.IsMatch(barcode))
+                {
+                    char[] digits = barcode.Where(x => char.IsDigit(x)).ToArray();
+
+                    if (digits.Length == 0)
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", digits)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid barcode");
+                }
+            }
+        }
+    }
+}
