b690a45b38affa789f3a540a7bd861b8150a6d57
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 20:58:41 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/ProgrammingFundamentalsMidExam/01.GuineaPig/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.GuineaPig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double food = double.Parse(Console.ReadLine());
+            double hay = double.Parse(Console.ReadLine());
+            double cover = double.Parse(Console.ReadLine());
+            double kgs = double.Parse(Console.ReadLine());
+
+            double neeededFood = .300 * 30;
+            double neeededHay = 0;// (food - (neeededFood / 2)) * .05;
+
+            double tempFood = food;
+            double tempHay = hay;
+            for (int i = 1; i <= 15; i++)
+            {
+                tempFood -= .600;
+                neeededHay = tempFood * .05;
+                tempHay = tempHay - neeededHay;
+            }
+           
+            double neeededCover = 10 * (kgs / 3);
+
+            if (food > neeededFood && hay > neeededHay && cover > neeededCover)
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {food - neeededFood:F2}, Hay: {tempHay:F2}, Cover: {cover - neeededCover:F2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Merry must go to the pet store!");
+            }
+        }
+    }
+}
