5cf309886b5f0a48d992feac445deae7cfbecfbd
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:24:19 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/StopWatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(10);
+                Console.Clear();
+            }
+
+            sw.Stop();
+            TimeSpan timeTaken = sw.Elapsed;
+            Console.WriteLine(timeTaken);
+            sw.Restart();
+        }
+    }
+}
