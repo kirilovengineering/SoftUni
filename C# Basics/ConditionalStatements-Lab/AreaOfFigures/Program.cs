f1ff15f599a741dfaa2577d8a7b08635f71692b8
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:21:40 +0300

    Add files via upload

---
+++ C# Basics/ConditionalStatements-Lab/AreaOfFigures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+
+            if (figure == ("square"))
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine( "{0:F3}", a * a );
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine( "{0:F3}", a * b );
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                Console.WriteLine( "{0:F3}", Math.PI * Math.Pow(r, 2));
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                Console.WriteLine( "{0:F3}", (a * ha) / 2 );
+            }
+        }
+    }
+}
---
+++ C# Basics/ConditionalStatements-Lab/SpeedInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SpeedInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// При скорост до 10(включително) отпечатайте & quot; slow & quot;
+// При скорост над 10 и до 50(включително) отпечатайте & quot; average & quot;
+// При скорост над 50 и до 150(включително) отпечатайте & quot; fast & quot;
+// При скорост над 150 и до 1000(включително) отпечатайте & quot; ultra fast&quot;
+// При по-висока скорост отпечатайте &quot; extremely fast&quot;
+            double a = double.Parse(Console.ReadLine());
+            if (a <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if ((a > 10) && (a <= 50))
+            {
+                Console.WriteLine("average");
+            }
+            else if ((a > 50) && (a <= 150))
+            {
+                Console.WriteLine("fast");
+            }
+            else if ((a > 150) && (a <= 1000))
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if ((a > 1000))
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
