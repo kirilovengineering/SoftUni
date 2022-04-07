a9b0a33622471c2da5e57f699722b3f1fe0fe012
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:51:32 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/ExamPreparationFinal3/02.DestinationMapper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"(=|\/)[A-Z][a-zA-Z]{2,}(\1)");
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> places = new List<string>();
+            int length = 0;
+
+            foreach (Match match in matches)
+            {
+
+                string word = match.Value;
+                word = word.Trim(new Char[] { '=', '/' });
+                places.Add(word);
+                length += word.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", places)}");
+            Console.WriteLine($"Travel Points: {length}");
+            
+
+        }
+    }
+}
