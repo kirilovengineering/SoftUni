6384d6af5c83cd2ae8434f497654eeb01945cc23
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:38:49 +0300

    Add files via upload

---
+++ C# Basics/PrepExam2/Barcode2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Barcode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            int startNumOnes = startNum / 1 % 10;
+            int startNumTens = startNum / 10 % 10;
+            int startNumHundreds = startNum / 100 % 10;
+            int startNumThousands = startNum / 1000;
+
+            int endNumOnes = endNum / 1 % 10;
+            int endNumTens = endNum / 10 % 10;
+            int endNumHundreds = endNum / 100 % 10;
+            int endNumThousands = endNum / 1000;
+
+            for (int i = startNumThousands; i <= endNumThousands; i++)
+            {
+                for (int j = startNumHundreds; j <= endNumHundreds; j++)
+                {
+                    for (int k = startNumTens; k <= endNumTens; k++)
+                    {
+                        for (int l = startNumOnes; l <= endNumOnes; l++)
+                        {
+                            if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Basics/PrepExam2/BarcodeGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BarcodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string barcodeOne = Console.ReadLine();
+            string barcodeTwo = Console.ReadLine();
+
+            string firstNumOne = $"{barcodeOne[0]}";
+            string firstNumTwo = $"{barcodeTwo[0]}";
+            string secondNumOne = $"{barcodeOne[1]}";
+            string secondNumTwo = $"{barcodeTwo[1]}";
+            string thirdNumOne = $"{barcodeOne[2]}";
+            string thirdNumTwo = $"{barcodeTwo[2]}";
+            string fourthNumOne = $"{barcodeOne[3]}";
+            string fourthNumTwo = $"{barcodeTwo[3]}";
+
+            for (int i = int.Parse(firstNumOne); i <= int.Parse(firstNumTwo); i++)
+            {
+                for (int j = int.Parse(secondNumOne); j <= int.Parse(secondNumTwo); j++)
+                {
+                    for (int k = int.Parse(thirdNumOne); k <= int.Parse(thirdNumTwo); k++)
+                    {
+                        for (int l = int.Parse(fourthNumOne); l <= int.Parse(fourthNumTwo); l++)
+                        {
+                            if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
