7704e8009a1555326d466f37e9b792aecf603e6f
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:23:25 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/SystemTextEncodingUTF8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SystemTextEncodingUTF8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            char ch = '\u00B1';
+            Console.WriteLine(ch);//This is +-
+
+        }
+    }
+}
