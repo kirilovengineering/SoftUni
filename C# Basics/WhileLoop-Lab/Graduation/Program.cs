2c42d44aa1b861927a3f79d8bd883325359e032f
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:42:20 +0300

    Add files via upload

---
+++ C# Basics/WhileLoop-Lab/Graduation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int grades = 1;
+            double currentGrade = 0;
+            double gradesSum = 0;
+            bool graduated = true;
+            int failCounter = 0;
+
+            while (grades <= 12)
+            {
+                currentGrade = double.Parse(Console.ReadLine());
+                if (currentGrade < 4)
+                {
+                    failCounter++;
+                    if (failCounter > 1)
+                    {
+                        Console.WriteLine($"{studentName} has been excluded at {grades} grade");
+                        graduated = false;
+                        break;
+                    }
+
+                    continue;
+                }
+
+                gradesSum += currentGrade;
+                grades++;
+            }
+
+            if (graduated)
+            {
+                Console.WriteLine($"{studentName} graduated. Average grade: {(gradesSum / 12):F2}");
+            }
+        }
+    }
+}
