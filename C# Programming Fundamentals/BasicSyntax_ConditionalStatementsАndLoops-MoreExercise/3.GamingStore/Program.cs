908eb2b267332e788ababb2f0306378b31c205ac
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:00:18 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/BasicSyntax_ConditionalStatementsАndLoops-MoreExercise/3.GamingStore/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace _3.GamingStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = double.Parse(Console.ReadLine()), total = 0;
+            string command = Console.ReadLine();
+
+            while (command != "Game Time")
+            {
+
+                if (balance == 0)
+                    break;
+
+                switch (command)
+                {
+                    case "OutFall 4":
+                        if (balance < 39.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 39.99;
+                            total += 39.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    case "CS: OG":
+
+                        if (balance < 15.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 15.99;
+                            total += 15.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    case "Zplinter Zell":
+
+                        if (balance < 19.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 19.99;
+                            total += 19.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    case "Honored 2":
+                        if (balance < 59.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 59.99;
+                            total += 59.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    case "RoverWatch":
+                        if (balance < 29.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 29.99;
+                            total += 29.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    case "RoverWatch Origins Edition":
+                        if (balance < 39.99)
+                        {
+                            Console.WriteLine("Too Expensive");
+                        }
+                        else
+                        {
+                            balance -= 39.99;
+                            total += 39.99;
+                            Console.WriteLine($"Bought {command}");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Not Found");
+                        continue;
+                }
+                command = Console.ReadLine();
+            }
+            if (balance == 0)
+            {
+                Console.WriteLine("Out of money!");
+            }
+            else
+            {
+                Console.WriteLine($"Total spent: ${total:f2}. Remaining: ${balance:f2}");
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals/BasicSyntax_ConditionalStatementsАndLoops-MoreExercise/4.ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _4.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            int n = input.Length;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
