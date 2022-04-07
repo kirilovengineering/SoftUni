9e9920223a278e2619049eca86c71796dbca2e24
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:46:32 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/LabIntroАndBasicSyntax/ForeignLanguages/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var country = Console.ReadLine();
+
+            switch (country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English"); break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish"); break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals/LabIntroАndBasicSyntax/MultiplicationTable2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiplicationTable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{n} X {multiplier} = {n * multiplier}");
+            }
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
