a86a8e9d2f7bac8f3456c5e72c4afe004aed8f0a
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:07:29 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/Arrays-MoreExercises/01.EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.EncryptSortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arrayWords = new string[n];
+            int[] arrayWordsCript = new int[n];
+            int[] arraySorted = new int[n];
+            string arrayWordsString = string.Empty;
+
+            int sumWordsVowel = 0; 
+            int sumWordsConsonant = 0;
+            int sumWords = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                arrayWords[i] = Console.ReadLine();
+
+                arrayWordsString = arrayWords[i];
+                for (int j = 0; j < arrayWordsString.Length; j++)
+                {
+                    char c = arrayWordsString[j];
+                    bool isVowel = "aeiou".IndexOf(c.ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
+                    if (isVowel)
+                    {
+                        sumWordsVowel += ((int)arrayWordsString[j]) * arrayWordsString.Length;
+                    }
+                    else
+                    {
+                        sumWordsConsonant += ((int)arrayWordsString[j]) / arrayWordsString.Length;
+                    }
+                }
+                sumWords = sumWordsVowel + sumWordsConsonant;
+                arrayWordsCript[i] = sumWords;
+                sumWordsVowel = 0;
+                sumWordsConsonant = 0;
+                sumWords = 0;
+            }
+
+            arraySorted = arrayWordsCript.OrderByDescending(c => c).ToArray();
+
+            for (int i = arraySorted.Length - 1; i >= 0 ; i--)
+            {
+                Console.WriteLine(arraySorted[i]);
+            }
+        }
+    }
+}
---
+++ C# Programming Fundamentals/Arrays-MoreExercises/02.PascalTriangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int value = 1;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int column = 0; column <= row; column++)
+                {
+                    if (column == 0 || row == 0)
+                    {
+                        value = 1;
+                    }
+                    else
+                    {
+                        value = value * (row - column + 1) / column;
+                    }
+
+                    if (column < row)
+                    {
+                        Console.Write(value + " ");
+                    }
+                    else
+                    {
+                        Console.Write(value + "");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
