ab8069311eda744a85f13ec5615fce278c1af760
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:10:09 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/Arrays-Exercise/2.CommonElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] first = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+            string[] second = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+
+            int length = 0;
+
+            if (first.Length > second.Length)
+            {
+                length = first.Length;
+            }
+            else
+            {
+                length = second.Length;
+            }
+
+            string[] common = new string[length];
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (first[i] == second[j])
+                    {
+                        common[j] = first[i];
+                    }
+                }
+
+            }
+            Console.WriteLine(String.Join(" ", common.Where(s => !string.IsNullOrEmpty(s))));
+        }
+    }
+}
