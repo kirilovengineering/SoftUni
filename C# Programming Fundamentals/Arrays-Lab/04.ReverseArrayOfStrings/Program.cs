b67335027a66f06d0e5e4fa92384d690ab880d85
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 22:09:09 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = Console.ReadLine().Split();
+
+            Array.Reverse(items);
+
+            Console.WriteLine(string.Join(" ", items));
+        }
+    }
+}
