00e975549097fe73a1e111c6641de5ae53a79b96
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:46:52 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/InspectLoopForToAndAfter/InspectLoopForToAndAfterArary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InspectLoopForToAndAfterArary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //Valid record in C# dinamicly length in array
+            int[] nums = new int[n];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    //Elements befor current index i;
+                }
+
+                for (int j = i; j < nums.Length; j++)
+                {
+                    //Elements after current index i;
+                }
+            }
+        }
+    }
+}
