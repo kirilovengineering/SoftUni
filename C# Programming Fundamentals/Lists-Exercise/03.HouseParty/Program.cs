659cfb479b9cb7c8abc81cbe5150264d89ab00f6
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-07 21:43:53 +0300

    Add files via upload

---
+++ C# Programming Fundamentals/Lists-Exercise/03.HouseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] message = Console.ReadLine().Split();
+                string name = message[0];
+
+                if (message[2] == "not")
+                {
+                    if (guests.Contains(name))
+                    {
+                        guests.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (!guests.Contains(name))
+                    {
+                        guests.Add(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, guests));
+        }
+    }
+}
---
+++ C# Programming Fundamentals/Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                                         .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                                         .Reverse()
+                                         .ToList();
+            List<int> nums = new List<int>();
+
+            foreach (string str in numbers)
+            {
+                nums.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                               .Select(int.Parse)
+                                                               .ToList());
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
