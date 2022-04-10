8ac54257e6b175592f8fb06b813ab17611295184
Kiril Kirilov <91634366+kirilovengineering@users.noreply.github.com>
2022-04-10 22:18:37 +0300

    Add files via upload

---
+++ C# Basics/WhileLoop-Exercise/OldBooks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookName = Console.ReadLine();
+            int countBooks = 0;
+            bool isFound = false;
+            string currentBook = Console.ReadLine();
+            while (currentBook != "No More Books")
+            {
+                if (currentBook == bookName)
+                {
+                    isFound = true;
+                    break;
+                }
+                else
+                {
+                    countBooks++;
+                }
+                currentBook = Console.ReadLine();
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"You checked {countBooks} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {countBooks} books.");
+            }
+        }
+    }
+}
