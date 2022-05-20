0a8887e75501bc3a0d75d73d83a9cfcbdc31f6dd
kirilovengineering <kirilovengineering@gmail.com>
2022-05-20 18:38:33 +0300

    Update 04

--- C# Advanced/StacksAndQueues-Lab/04. Matching Brackets/Program.cs
+++ C# Advanced/StacksAndQueues-Lab/04. Matching Brackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04._Matching_Brackets
 {
@@ -6,7 +7,25 @@ namespace _04._Matching_Brackets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string expression = Console.ReadLine();
+           var stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                if (c == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (c == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpression = expression.Substring(startIndex, endIndex - startIndex +1);
+                    Console.WriteLine(subExpression);
+                }
+
+            }
         }
     }
 }
