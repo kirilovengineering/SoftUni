a61e2b6dc324aabae37d1a0c6ec547915ba3bc60
kirilovengineering <kirilovengineering@gmail.com>
2022-05-20 15:21:01 +0300

    Update02

--- C# Advanced/StacksAndQueues-Lab/02. Stack Sum/Program.cs
+++ C# Advanced/StacksAndQueues-Lab/02. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Stack_Sum
 {
@@ -6,7 +8,41 @@ namespace _02._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(elements);
+
+            while (true)
+            {
+                string command = Console.ReadLine().ToLower();
+                string[] commandItems = command.Split(' ');
+
+                if (commandItems[0] == "add")
+                {
+                    int n1 = int.Parse(commandItems[1]);
+                    int n2 = int.Parse(commandItems[2]);
+                    stack.Push(n1);
+                    stack.Push(n2);
+                }
+                else if (commandItems[0] == "remove")
+                {
+                    int count = int.Parse(commandItems[1]);
+                    if (stack.Count >= count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                else if (commandItems[0] == "end")
+                {
+                    var sum = stack.ToArray().Sum();
+                    Console.WriteLine($"Sum: {sum}");
+                    break;
+                }
+            }
         }
     }
 }
