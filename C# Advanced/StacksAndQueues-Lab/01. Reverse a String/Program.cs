8b6ca5f96d2ca93d0cfafbbcc166b630e76fe04d
kirilovengineering <kirilovengineering@gmail.com>
2022-05-20 14:35:02 +0300

    Update01

--- C# Advanced/StacksAndQueues-Lab/01. Reverse a String/Program.cs
+++ C# Advanced/StacksAndQueues-Lab/01. Reverse a String/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Reverse_a_String
 {
@@ -6,7 +7,19 @@ namespace _01._Reverse_a_String
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputString = Console.ReadLine();    
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in inputString)
+            { 
+            stack.Push(c);
+            }
+
+            while (stack.Count > 0)
+            { 
+            Console.Write(stack.Pop());
+            }
         }
     }
 }
