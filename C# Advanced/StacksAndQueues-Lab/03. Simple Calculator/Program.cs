f1c9a4df5d86279b2266de1954321b5075066b46
kirilovengineering <kirilovengineering@gmail.com>
2022-05-20 16:29:51 +0300

    Update 03

--- C# Advanced/StacksAndQueues-Lab/03. Simple Calculator/Program.cs
+++ C# Advanced/StacksAndQueues-Lab/03. Simple Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03._Simple_Calculator
 {
@@ -6,7 +7,38 @@ namespace _03._Simple_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] elements = Console.ReadLine().Split(' ');
+            Stack<string> input = new Stack<string>(elements);
+
+            int sum = 0;
+            string sign = string.Empty;
+
+            while (input.Count > 0)
+            {
+                int value = int.Parse(input.Peek());   
+                input.Pop();
+                if (input.Count != 0)
+                {
+                    sign = input.Peek();
+                    input.Pop();
+                }
+                else
+                {
+                    sum = sum + value;
+                }
+
+                if (sign == "+")
+                {
+                    sum = sum + value;
+                }
+                else if (sign == "-")
+                {
+                    sum = sum - value;
+                }
+                sign = string.Empty;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
