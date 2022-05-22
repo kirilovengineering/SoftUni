a4fbfb4b3cf3c4cd904b344c4bc13b530f199014
kirilovengineering <kirilovengineering@gmail.com>
2022-05-22 12:51:23 +0300

    100/100

--- C# Advanced/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
+++ C# Advanced/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.SquaresInMatrix
 {
@@ -6,7 +7,39 @@ namespace _02.SquaresInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] dimensions = Console.ReadLine()
+                                      .Split(" ")
+                                      .Select(int.Parse)
+                                      .ToArray();
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] symbols = Console.ReadLine()
+                                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(char.Parse) 
+                                        .ToArray();
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[row, i] = symbols[i];
+                }
+            }
+
+            int squareMatrices = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int i = 0; i < matrix.GetLength(1) - 1; i++)
+                {
+                    if (matrix[row, i] == matrix[row, i + 1] &&
+                        matrix[row, i] == matrix[row + 1, i] &&
+                        matrix[row, i] == matrix[row + 1, i + 1])
+                    {
+                        squareMatrices++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squareMatrices);
         }
     }
 }
