9658486e7e629705dbc93e93993b2631545c0d7b
kirilovengineering <kirilovengineering@gmail.com>
2022-05-22 13:47:51 +0300

    100/100

--- C# Advanced/MultidimensionalArrays-Exercise/07.KnightGame/Program.cs
+++ C# Advanced/MultidimensionalArrays-Exercise/07.KnightGame/Program.cs
@@ -6,7 +6,152 @@ namespace _07.KnightGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //You will receive a board with K for knights and '0' for empty cells.
+            //Your task is to remove a minimum of the knights,
+            //so there will be no knights left that can attack another knight.
+            //Конят се придвижва до най-близкото поле, но не на същия ред, колона или диагонал.
+            //(Това може да се мисли като преместване на два квадрата хоризонтално,
+            //след това един квадрат вертикално, или преместване на един квадрат хоризонтално,
+            //след това два квадрата вертикално – т.е. в „L“ модел.)
+            //Играта на рицар се играе на дъска с размери N x N и много шах рицари(офицери) 0 <= K <= N2.
+            //Ще получите дъска с 'K' рицари и '0' за празни клетки.
+            //Вашата задача е да премахнете минимум рицари,
+            //така че да не останат рицари, които да атакуват друг рицар.
+
+            //Output
+            //Print a single integer with the minimum number of knights that needs to be removed
+
+
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowElements = Console.ReadLine().ToCharArray();
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[row, i] = rowElements[i];
+                }
+            }     
+                                                                          //   Невалидни полета   
+                                                                          //       0   1   2   3   4   COL
+            int attackableKnights = 0;                                    // ROW ---------------------                                      
+            int maxAttackableKnights = -1;                                //  0  |   | X |   | X |   |      K --> Knight
+            int bestKnightRow = 0;                                        //  1  | X |   |   |   | X |      X --> Attackable Unit (Other Knight)
+            int bestKnightCol = 0;                                        //  2  |   |   | K |   |   |
+            int count = 0;                                                //  3  | X |   |   |   | A |
+                                                                          //  4  |   | X |   | X |   |
+            while (true)                                                  //     ---------------------
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int i = 0; i < matrix.GetLength(1); i++)
+                    {
+                        if (matrix[row, i] == 'K')
+                        {
+                            //--------- 1 row UNDER ---------
+                            if (row + 1 < matrix.GetLength(0))
+                            {
+                                if (i - 2 >= 0)
+                                {
+                                    if (matrix[row + 1, i - 2] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+
+                                if (i + 2 < matrix.GetLength(1))
+                                {
+                                    if (matrix[row + 1, i + 2] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+                            }
+
+                            //--------- 1 row UP ---------
+                            if (row - 1 >= 0)
+                            {
+                                if (i - 2 >= 0)
+                                {
+                                    if (matrix[row - 1, i - 2] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+
+                                if (i + 2 < matrix.GetLength(1))
+                                {
+                                    if (matrix[row - 1, i + 2] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+                            }
+                            //--------- 2 rows UNDER ---------
+                            if (row + 2 < matrix.GetLength(0))
+                            {
+                                if (i + 1 < matrix.GetLength(1))
+                                {
+                                    if (matrix[row + 2, i + 1] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+
+                                if (i - 1 >= 0)
+                                {
+                                    if (matrix[row + 2, i - 1] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+                            }
+
+                            //--------- 2 rows UP ---------
+                            if (row - 2 >= 0)
+                            {
+                                if (i + 1 < matrix.GetLength(1))
+                                {
+                                    if (matrix[row - 2, i + 1] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+
+                                if (i - 1 >= 0)
+                                {
+                                    if (matrix[row - 2, i - 1] == 'K')
+                                    {
+                                        attackableKnights++;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (attackableKnights > maxAttackableKnights)
+                        {
+                            maxAttackableKnights = attackableKnights;
+                            bestKnightRow = row;
+                            bestKnightCol = i;
+                        }
+                        attackableKnights = 0;
+                    }
+                }
+
+                if (maxAttackableKnights != 0)
+                {
+                    matrix[bestKnightRow, bestKnightCol] = '0';
+                    maxAttackableKnights = 0;
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    return;
+                }
+            }
         }
     }
 }
---
+++ C# Advanced/MultidimensionalArrays-Exercise/IsValidMoveInMatrix/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace IsValidMoveInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Матрицата е правоъглна област от определен брой клетни в дължина и ширина.
+            //Матрица с размери row = 5 и col = 7 е матрица със 5 реда и 7 колони:
+
+            int[,] matrix = new int[5, 7];
+
+            int maxRowLength = matrix.GetLength(0);
+            int maxColLength = matrix.GetLength(1);
+
+            int row = 2;
+            int col = 3;
+
+            string command = Console.ReadLine();
+
+            int stepRow = 0;
+            int stepCol = 0;
+
+            while (command != "end")
+            {
+
+
+                switch (command)
+                {
+                    case "left":
+                        stepCol--;
+                        break;
+                    case "right":
+                        stepCol++;
+                        break;
+                    case "up":
+                        stepRow--;
+                        break;
+                    case "down":
+                        stepRow++;
+                        break;
+                    default:
+                        break;
+                }
+
+                int newRow = row + stepRow;
+                int newCol = col + stepCol;
+
+                if (IsNewPositionValid(maxRowLength, maxColLength, newRow, newCol))
+                {
+                    // TODO: Изпълнение логиката на задачата
+
+                    row = newRow;
+                    col = newCol;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static bool IsNewPositionValid(int maxRowLength, int maxColLength, int newRow, int newCol)
+        {
+            bool result = ((newRow >= 0) && (newRow < maxRowLength));
+            result = ((result) && (newCol >= 0) && (newCol < maxColLength));
+            return result;
+        }
+    }
+}
