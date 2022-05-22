c83801193de477109c6340e184a55f76ef3b1810
kirilovengineering <kirilovengineering@gmail.com>
2022-05-22 12:31:10 +0300

    1

--- C# Advanced/MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
+++ C# Advanced/MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.PrimaryDiagonal
 {
@@ -6,7 +7,29 @@ namespace _03.PrimaryDiagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                                       .Split(" ")
+                                       .Select(int.Parse)
+                                       .ToArray();
+
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[row, i] = numbers[i];
+                }
+            }
+
+            int diagonalSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                diagonalSum += matrix[i, i];
+            }
+
+            Console.WriteLine(diagonalSum);
         }
     }
 }
--- C# Advanced/MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
+++ C# Advanced/MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
@@ -6,7 +6,34 @@ namespace _04.SymbolInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] symbols = Console.ReadLine().ToCharArray();
+
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[row, i] = symbols[i];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    if (matrix[row, i] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {i})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
         }
     }
 }
