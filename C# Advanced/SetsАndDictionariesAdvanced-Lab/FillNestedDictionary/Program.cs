41ea632a856e59be654eda5e558ddfdd8d05ee14
kirilovengineering <kirilovengineering@gmail.com>
2022-05-24 23:35:23 +0300

    100/100

--- C# Advanced/SetsАndDictionariesAdvanced-Lab/FillNestedDictionary/Program.cs
+++ C# Advanced/SetsАndDictionariesAdvanced-Lab/FillNestedDictionary/Program.cs
@@ -7,21 +7,47 @@ namespace FillNestedDictionary
     {
         static void Main(string[] args)
         {
-            var population = new Dictionary<string, Dictionary<string, int>> {
-    {"BG",
-        new Dictionary<string, int> {
-            { "Sofia", 1_211_000 },
-            { "Plovdiv", 338_000 },
-            { "Varna", 335_000 },
-        }
-    },
-    {"UK",
-        new Dictionary<string, int> {
-            { "London", 8_674_000 },
-            { "Manchester", 2_550_000 },
-        }
-    }, …
-};
+            var population = new Dictionary<string, Dictionary<string, int>>
+            {
+                {"BG",
+                     new Dictionary<string, int>
+                    {
+                        { "Sofia", 1_211_000 }, // подчертавките служат за разделяне визуално е по удоно, същата цифра си остава като такава
+                        { "Plovdiv", 338_000 },
+                        { "Varna", 335_000 },
+                    }
+                },
+                {"UK",
+                        new Dictionary<string, int>
+                    {
+                        { "London", 8_674_000 },
+                        { "Manchester", 2_550_000 },
+                    }
+                }
+            };
+
+            //Nested Dictionaries: Printing
+            foreach (var (countryName, towns) in population)
+            {
+                Console.WriteLine("Country: " + countryName);
+                foreach (var (townName, townPop) in towns)
+                    Console.WriteLine(
+                       $"\tTown {townName} --> population {townPop}");
+            }
+
+            //Nested Dictionaries: Adding New Entry with method
+
+            AddPopulation("China", "Shanghai", 24_300_000);
+            AddPopulation("China", "Beijing", 18_800_000);
+            AddPopulation("China", "Shenzhen", 12_700_000);
+            AddPopulation("BG", "Stara Zagora", 250_000);
+
+            void AddPopulation(string country, string town, int townPop)
+            {
+                if (!population.ContainsKey(country))
+                    population[country] = new Dictionary<string, int>();
+                population[country][town] = townPop;
+            }
 
         }
     }
