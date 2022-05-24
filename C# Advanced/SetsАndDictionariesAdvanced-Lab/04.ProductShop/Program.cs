96979527d75b7b1b890e2728952458613423e436
kirilovengineering <kirilovengineering@gmail.com>
2022-05-25 00:11:10 +0300

    100/100

--- C# Advanced/SetsАndDictionariesAdvanced-Lab/04.ProductShop/Program.cs
+++ C# Advanced/SetsАndDictionariesAdvanced-Lab/04.ProductShop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.ProductShop
 {
@@ -6,7 +8,42 @@ namespace _04.ProductShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prices = new Dictionary<string, Dictionary<string, double>>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "Revision")
+                {
+                    PrintPrices(prices);
+                    break;
+                }
+                string[] parts = line.Split(", ");
+                string shop = parts[0];
+                string product = parts[1];
+                double price = double.Parse(parts[2]);
+                AddProduct(prices, shop, product, price);
+            }
+        }
+
+        private static void AddProduct(Dictionary<string, Dictionary<string, double>> prices, string shop, string product, double price)
+        {
+            if (!prices.ContainsKey(shop))
+                prices.Add(shop, new Dictionary<string, double>());
+            prices[shop][product] = price;
+        }
+
+        private static void PrintPrices(Dictionary<string, Dictionary<string, double>> prices)
+        {
+            foreach (var shopAndProducts in prices.OrderBy(sp => sp.Key))
+            {
+                string shopName = shopAndProducts.Key;
+                Console.WriteLine(shopName + "->");
+                var products = shopAndProducts.Value;
+                foreach (var iproductAndPrice in products)
+                {
+                    Console.WriteLine($"Product: {iproductAndPrice.Key}, Price: {iproductAndPrice.Value}");
+                }
+            }
         }
     }
 }
