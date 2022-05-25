363da351f6b0b29b8aa5667e2b82d0d1bdcfeccb
kirilovengineering <kirilovengineering@gmail.com>
2022-05-25 19:13:05 +0300

    100/100

--- C# Advanced/SetsАndDictionariesAdvanced-Lab/07.ParkingLot/Program.cs
+++ C# Advanced/SetsАndDictionariesAdvanced-Lab/07.ParkingLot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07.ParkingLot
 {
@@ -6,7 +7,30 @@ namespace _07.ParkingLot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> cars = new HashSet<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(", ");
+                switch (tokens[0])
+                {
+                    case "IN":
+                        cars.Add(tokens[1]);
+                        break;
+                    case "OUT":
+                        cars.Remove(tokens[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(cars.Count != 0 ? $"{String.Join(Environment.NewLine, cars)}" : "Parking Lot is Empty");
         }
     }
 }
--- C# Advanced/SetsАndDictionariesAdvanced-Lab/08.SoftUniParty/Program.cs
+++ C# Advanced/SetsАndDictionariesAdvanced-Lab/08.SoftUniParty/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08.SoftUniParty
 {
@@ -6,7 +7,45 @@ namespace _08.SoftUniParty
     {
         static void Main(string[] args)
         {
-           
+            HashSet<string> vips = new HashSet<string>();
+            HashSet<string> guests = new HashSet<string>();
+            string command = Console.ReadLine();
+            while (command != "PARTY")
+            {
+                if (Char.IsDigit(command[0]))
+                {
+                    vips.Add(command);
+                }
+                else
+                {
+                    guests.Add(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (Char.IsDigit(command[0]))
+                {
+                    vips.Remove(command);
+                }
+                else
+                {
+                    guests.Remove(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count + vips.Count);
+            foreach (var item in vips)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in guests)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
