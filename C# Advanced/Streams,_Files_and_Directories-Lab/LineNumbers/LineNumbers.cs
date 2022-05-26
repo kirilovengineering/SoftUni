2d32d71316bcbcfadb79a7e2296658d28e8c3f84
kirilovengineering <kirilovengineering@gmail.com>
2022-05-26 20:41:10 +0300

    Stream

---
+++ C# Advanced/Streams,_Files_and_Directories-Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,19 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            
+        }
+    }
+}
---
+++ C# Advanced/Streams,_Files_and_Directories-Lab/OddLines/OddLines.cs
@@ -0,0 +1,21 @@
+﻿
+namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputPath, outputPath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+           
+        }
+    }
+}
