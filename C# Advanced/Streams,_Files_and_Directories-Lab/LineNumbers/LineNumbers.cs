441519767034c56f9fc5fb4d1747e9437c0b9ce2
kirilovengineering <kirilovengineering@gmail.com>
2022-05-27 00:00:05 +0300

    last one

--- C# Advanced/Streams,_Files_and_Directories-Lab/LineNumbers/LineNumbers.cs
+++ C# Advanced/Streams,_Files_and_Directories-Lab/LineNumbers/LineNumbers.cs
@@ -5,15 +5,29 @@
     {
         static void Main(string[] args)
         {
-            string inputPath = @"..\..\..\Files\input.txt";
-            string outputPath = @"..\..\..\Files\output.txt";
+            string inputPath = @"../../../Files/input.txt"; // дясната черта(която си е линукска) работи и на уидоус
+            string outputPath = @"../../../Files/output.txt";
 
             RewriteFileWithLineNumbers(inputPath, outputPath);
         }
 
         public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
         {
-            
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNum = 1;
+                    string line = string.Empty;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(lineNum + ". " + line);
+                        lineNum++;
+                    }
+                }
+            }
         }
     }
 }
---
+++ C# Advanced/Streams,_Files_and_Directories-Lab/TryCatchFinally/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace TryCatchFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  в случая ще отпечата ексепшън защото няма да намери input.txt
+
+            StreamReader reader = null;
+            int linesCount = 0;
+            try // опитваме да прочетем файл, ако прочетем обработваме
+            {
+                reader = new StreamReader("input.txt");
+                while (reader.ReadLine() != null)
+                    linesCount++;
+                Console.WriteLine("Lines count: {0}", linesCount);
+            }
+            catch (Exception ex) // ако не успеем хващаме грешка и отпечатваме .. грешка
+            {
+                Console.Error.WriteLine("Error reading file: {0}", ex);
+            }
+            finally
+            {
+                if (reader != null) reader.Close(); // накрая задължително затваряме стриима
+            }
+
+        }
+    }
+}
