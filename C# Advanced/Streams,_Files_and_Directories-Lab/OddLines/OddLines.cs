868c4d4859a486558f3dba80e74e32d79d508e7c
kirilovengineering <kirilovengineering@gmail.com>
2022-05-26 21:46:02 +0300

    100/100 Odd Lines

--- C# Advanced/Streams,_Files_and_Directories-Lab/OddLines/OddLines.cs
+++ C# Advanced/Streams,_Files_and_Directories-Lab/OddLines/OddLines.cs
@@ -15,7 +15,24 @@ namespace OddLines
 
         public static void ExtractOddLines(string inputFilePath, string outputFilePath)
         {
-           
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNum = 0;
+                    string line = string.Empty;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (lineNum % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        lineNum++;
+                    }
+                }
+            }
         }
     }
 }
