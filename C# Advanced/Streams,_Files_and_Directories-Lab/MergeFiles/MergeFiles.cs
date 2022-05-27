e27ac8641a2c18b91b299f1831c201b385cfd76e
kirilovengineering <kirilovengineering@gmail.com>
2022-05-27 22:32:10 +0300

    100/100

--- C# Advanced/Streams,_Files_and_Directories-Lab/MergeFiles/MergeFiles.cs
+++ C# Advanced/Streams,_Files_and_Directories-Lab/MergeFiles/MergeFiles.cs
@@ -6,16 +6,38 @@
     {
         static void Main(string[] args)
         {
-            var firstInputFilePath = @"..\..\..\Files\input1.txt";
-            var secondInputFilePath = @"..\..\..\Files\input2.txt";
-            var outputFilePath = @"..\..\..\Files\output.txt";
+            var firstInputFilePath = @"../../../Files/input1.txt";
+            var secondInputFilePath = @"../../../Files/input2.txt";
+            var outputFilePath = @"../../../Files/output.txt";
 
             MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
         }
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            string[] file1 = File.ReadAllLines(firstInputFilePath);
+            string[] file2 = File.ReadAllLines(secondInputFilePath);
             
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            using (writer)
+            {
+                int lineNumber = 0;
+                while (lineNumber < file1.Length || lineNumber < file2.Length)
+                {
+                    if (lineNumber < file1.Length)
+                    {
+                        writer.WriteLine(file1[lineNumber]);
+                    }
+
+                    if (lineNumber < file2.Length)
+                    {
+                        writer.WriteLine(file2[lineNumber]);
+                    }
+
+                    lineNumber++;
+                }
+            }
         }
+
     }
 }
