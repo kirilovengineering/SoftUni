a203e37a8f9fca4f5917d0e14bd0705b7a57d171
kirilovengineering <kirilovengineering@gmail.com>
2022-05-29 22:14:11 +0300

    100/100 Judge in Directory Skeleton

--- C# Advanced/Streams,_Files_and_Directories_Exercise/Skeleton/LineNumbers/LineNumbers.cs
+++ C# Advanced/Streams,_Files_and_Directories_Exercise/Skeleton/LineNumbers/LineNumbers.cs
@@ -1,19 +1,41 @@
 ﻿namespace LineNumbers
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     public class LineNumbers
     {
         static void Main()
         {
-            string inputFilePath = @"..\..\..\text.txt";
-            string outputFilePath = @"..\..\..\output.txt";
+            string inputFilePath = @"../../../text.txt";
+            string outputFilePath = @"../../../output.txt";
 
             ProcessLines(inputFilePath, outputFilePath);
         }
 
         public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            throw new NotImplementedException();
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> outputLines = new List<string>();
+
+            foreach (string line in lines)
+            {
+                //"-I was quick to judge him, but it wasn't his fault."
+                count++;
+
+                //2. броим буквите
+                int countLetters = line.Count(char.IsLetter); //37
+                //3. броим символите, които са пунктуационни знаци
+                int countSymbol = line.Count(char.IsPunctuation); //4
+                //1. номер на реда
+                string modifiedLine = $"Line {count}: {line} ({countLetters})({countSymbol})";
+                //"Line 1: -I was quick to judge him, but it wasn't his fault. (37)(4)"
+                outputLines.Add(modifiedLine);
+
+            }
+            File.WriteAllLines(outputFilePath, outputLines);
         }
     }
 }
