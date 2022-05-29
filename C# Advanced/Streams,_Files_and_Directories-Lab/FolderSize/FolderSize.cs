ec52846b4a06ca4a3ea52a450a9de576f9408e89
kirilovengineering <kirilovengineering@gmail.com>
2022-05-29 13:19:21 +0300

    Compile time error, GetFolderSize' takes 2 arguments [/tmp/ExecutionStrategies/jgfvaque.zoh/NUnitLiteConsoleApp/NUnitLiteConsoleApp.csproj]

--- C# Advanced/Streams,_Files_and_Directories-Lab/FolderSize/FolderSize.cs
+++ C# Advanced/Streams,_Files_and_Directories-Lab/FolderSize/FolderSize.cs
@@ -6,14 +6,37 @@
     {
         static void Main(string[] args)
         {
-            string folderPath = @"..\..\..\Files\TestFolder";
-            string outputPath = @"..\..\..\Files\output.txt";
+            string folderPath = @"../../../Files/TestFolder";
+            string outputPath = @"../../../Files/output.txt";
 
-            GetFolderSize(folderPath, outputPath);
+
+
+
+            double size = GetFolderSize(folderPath);
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                writer.WriteLine(size);
+            }
         }
 
-        public static void GetFolderSize(string folderPath, string outputFilePath)
+        public static double GetFolderSize(string folderPath)
         {
+            double size = 0;
+            string[] files = Directory.GetFiles(folderPath);
+            string[] subDirectories = Directory.GetDirectories(folderPath);
+
+            foreach (string subDirectory in subDirectories)
+            {
+                size += GetFolderSize(subDirectory);
+            }
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                size += fileInfo.Length;
+            }
+
+            return size / 1024;
         }
     }
 }
