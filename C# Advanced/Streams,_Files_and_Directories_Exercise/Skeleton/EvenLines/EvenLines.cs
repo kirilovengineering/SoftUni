dd25bf95c42ed234d94a8db45924331220a6d6c7
kirilovengineering <kirilovengineering@gmail.com>
2022-05-29 22:00:26 +0300

    P01_Even_Lines.zip 100/100 Judge

--- C# Advanced/Streams,_Files_and_Directories_Exercise/Skeleton/EvenLines/EvenLines.cs
+++ C# Advanced/Streams,_Files_and_Directories_Exercise/Skeleton/EvenLines/EvenLines.cs
@@ -1,18 +1,67 @@
 ﻿namespace EvenLines
 {
     using System;
+    using System.IO;
+    using System.Linq;
+
     public class EvenLines
     {
         static void Main()
         {
             string inputFilePath = @"..\..\..\text.txt";
 
-            Console.WriteLine(ProcessLines(inputFilePath));
+            ProcessLines(inputFilePath);
         }
 
         public static string ProcessLines(string inputFilePath)
         {
-            throw new NotImplementedException();
+            //1. прочетем четните редовете
+            //2. заменим символите с @
+            //3. reverse words
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = -1; //брой редовете
+                string line = reader.ReadLine(); //четем по 1 ред от файла
+
+                while (line != null)
+                {
+                    counter++;
+                    if (counter % 2 == 0)
+                    {
+                        //замяна
+                        line = Replace(line); /*line.Replace("-", "@")
+                                                     .Replace(",", "@")
+                                                     .Replace(".", "@")
+                                                     .Replace("!", "@")
+                                                     .Replace("?", "@");*/
+                        //обърна в обратен ред
+                        line = Reverse(line); /*string.Join(" ", line.Split().Reverse());*/
+                        Console.WriteLine(line);
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        private static string Reverse(string line)
+        {   //"@I was quick to judge him@ but it wasn't his fault@".Split()
+            //["@I", "was", "quick", "to", "judge", "him@", "but", "it", "wasn't", "his", "fault@"].Reverse()
+            //["fault@", "his", "wasn't", "it", "but", "him@", "judge", "to", "quick", "was", "@I"].Join(" ")
+            //"fault@ his wasn't it but him@ judge to quick was @I"
+            return string.Join(" ", line.Split().Reverse());
+        }
+
+        private static string Replace(string line) //върне реда със заместени символи
+        {
+            //-I was quick to judge him, but it wasn't his fault.
+            //@I was quick to judge him@ but it wasn't his fault@
+            return line.Replace("-", "@")
+                 .Replace(",", "@")
+                 .Replace(".", "@")
+                 .Replace("!", "@")
+                 .Replace("?", "@");
         }
     }
 }
