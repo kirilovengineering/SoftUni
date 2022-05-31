96220b25d2029325571c962b9c0a4e25c270b66a
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 21:40:55 +0300

    Functional Programin03.CountUppercaseWordsg

--- C# Advanced/FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
+++ C# Advanced/FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.CountUppercaseWords
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //static void Main(string[] args)
+        //{
+        //    Predicate<string> isUpperFirstLetter = 
+        //        (string x) => x.Length > 0 && char.IsUpper(x[0]);
+        //    Console.WriteLine(
+        //        String.Join(
+        //            "\r\n", //5. Рзпечатваме на конзола разделени по нов ред
+        //            Console.ReadLine() // 1. Четем от конзола
+        //            .Split(" ") // 2. Рзделените думи от конзолния стринг по интервал
+        //            .Where(x => isUpperFirstLetter(x)) //3. Където са думи започващи с главна буква
+        //            .ToArray() // 4. Към масив естествено.
+        //            )           
+        //        );
+        //}
+
+        // Типичен пример за функционално програмиране
+        static void Main() =>
+            Console.WriteLine(
+                String.Join(
+                    "\r\n", //5. Рзпечатваме на конзола разделени по нов ред
+                    Console.ReadLine() // 1. Четем от конзола
+                    .Split(" ") // 2. Рзделените думи от конзолния стринг по интервал
+                    .Where(x => x.Length > 0 && char.IsUpper(x[0])) //3. Където са думи започващи с главна буква
+                    .ToArray() // 4. Към масив естествено.
+                    )
+                );
+
+
     }
 }
--- C# Advanced/FunctionalProgramming-Lab/PredicateWhatIs/Program.cs
+++ C# Advanced/FunctionalProgramming-Lab/PredicateWhatIs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PredicateWhatIs
 {
@@ -6,8 +7,16 @@ namespace PredicateWhatIs
     {
         static void Main(string[] args)
         {
+            //In the. NET Predicate<T> is Boolean method
             // Предикатът е - > Взема параметър Т и бръща булеан
             Predicate<int> isNegative = x => x < 0; // Проверяваме входа инт х дали е по малко от нула.
+
+            Console.WriteLine(isNegative(5)); // false
+            Console.WriteLine(isNegative(-5)); // true
+
+            var nums = new List<int> {3, 5, -2, 10, 0 -3 };
+            var negs = nums.FindAll(isNegative);
+            Console.WriteLine(String.Join(", ", negs)); // -2, -3
         }
     }
 }
