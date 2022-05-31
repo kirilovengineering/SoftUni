fee1110cb8cf9668d50a7f0263a87ba89e257c77
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 20:02:38 +0300

    Delegate What Is

---
+++ C# Advanced/FunctionalProgramming-Lab/DelegateWhatIs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateWhatIs
+{
+    //Дефинираме като клас, в C# тип данни който съдържа метод с определени параметри и връща типа
+    // в случая приема x y и връща int
+    //Казваме Делегата Aggregat връща int 
+    // Делегата е call back method - метод извиква друг метод като параметър 
+    public delegate int Aggregate(int x, int y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aggregate multiplay = (x,y) => x * y;
+            Aggregate devide = (x, y) => x / y;
+            Aggregate add = (x, y) => x + y;
+
+            Console.WriteLine(multiplay(3, 5));
+            Console.WriteLine(devide(3, 5));
+            Console.WriteLine(add(3, 5));
+        }
+    }
+}
---
+++ C# Advanced/FunctionalProgramming-Lab/delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace internal delegate
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
