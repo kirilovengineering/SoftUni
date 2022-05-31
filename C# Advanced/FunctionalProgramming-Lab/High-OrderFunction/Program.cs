867396168ec1d184ce5ca10bf35fa74f5d965024
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 22:44:28 +0300

    High-OrderFunctionMoreExample

--- C# Advanced/FunctionalProgramming-Lab/High-OrderFunction/Program.cs
+++ C# Advanced/FunctionalProgramming-Lab/High-OrderFunction/Program.cs
@@ -9,18 +9,18 @@ namespace High_OrderFunction
 
         static void Main(string[] args)
         {
-            int Operation(int number, Func<int, int> operation)
-            {
-                return operation(number);
-            }
-            //We can use the method like that:
-            int a = 5;
-            int b = Operation(a, number => number * 5); // 25
-            Console.WriteLine(b);
-            int c = Operation(a, number => number - 3); // 2
-            Console.WriteLine(c);
-            int d = Operation(b, number => number % 2); // 1
-            Console.WriteLine(d);
+            //int Operation(int number, Func<int, int> operation)
+            //{
+            //    return operation(number);
+            //}
+            ////We can use the method like that:
+            //int a = 5;
+            //int b = Operation(a, number => number * 5); // 25
+            //Console.WriteLine(b);
+            //int c = Operation(a, number => number - 3); // 2
+            //Console.WriteLine(c);
+            //int d = Operation(b, number => number % 2); // 1
+            //Console.WriteLine(d);
         }
     }
 }
---
+++ C# Advanced/FunctionalProgramming-Lab/High-OrderFunctionMoreExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace High_OrderFunctionMoreExample
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            long Aggregate(int start, int end, Func<long, long, long> op)
+            {
+                long result = start;
+                for (int i = start + 1; i <= end; i++)
+                {
+                    result = op(result, i);
+                }
+                return result;
+
+            }
+
+            Console.WriteLine(Aggregate(1, 10, (a, b) => a + b)); // 55
+            Console.WriteLine(Aggregate(1, 10, (a, b) => a * b)); // 3628800
+            Console.WriteLine(Aggregate(1, 10, (a, b) => long.Parse("" + a + b))); // 12345678910
+        }
+    }
+}
