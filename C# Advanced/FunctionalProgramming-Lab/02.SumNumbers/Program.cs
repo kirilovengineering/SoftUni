e3029e947c2f579f71f22e36fe9c85500f59255b
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 21:08:32 +0300

    PredicateWhatIs

--- C# Advanced/FunctionalProgramming-Lab/02.SumNumbers/Program.cs
+++ C# Advanced/FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.SumNumbers
 {
     internal class Program
+
     {
+        // Вариант 1
+        static int Parse1(string str)
+        {
+            int result = int.Parse(str);
+            return result;
+        }
+        // Вариант 2
+        static int Parse2(string str) => int.Parse(str);
+
+        // няма значение кой вариант ще използваме
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static int Parse(string str) => int.Parse(str);
+
+            string input = Console.ReadLine();
+            int[] nums = input.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Parse)
+                .ToArray();
+
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
+
         }
     }
 }
---
+++ C# Advanced/FunctionalProgramming-Lab/PredicateWhatIs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PredicateWhatIs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Предикатът е - > Взема параметър Т и бръща булеан
+            Predicate<int> isNegative = x => x < 0; // Проверяваме входа инт х дали е по малко от нула.
+        }
+    }
+}
