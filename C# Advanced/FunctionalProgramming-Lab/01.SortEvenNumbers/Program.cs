36259a91ca0d5f750a8e3f4cf6d0122722cec526
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 19:36:34 +0300

    100/100

--- C# Advanced/FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
+++ C# Advanced/FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.SortEvenNumbers
 {
@@ -6,7 +7,31 @@ namespace _01.SortEvenNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] numbers = Console.ReadLine()
+            //    .Split(new String[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(n => int.Parse(n))//maping (преобразуване)функция - приема стринг => връща число
+            //    .Where(n => n % 2 == 0) //предикат - прима число => връща true, false
+            //    .OrderBy(n => n)//Селектрона фукция - взима обект и връща ключа, по който ще сортираме
+            //    .ToArray();
+            //string result = string.Join(", ", numbers);
+            //Console.WriteLine(result);
+
+
+            //Показателен пример за функционално програмиране разширено
+            Func<string, int> funcParsToInt = x => int.Parse(x);
+            Func<int, bool> isEven = x => x % 2 == 0;
+            Func<int, int> identity = x => x;
+
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int[] nums = tokens.Select(funcParsToInt).ToArray();
+            int[] evenNums = nums.Where(isEven).ToArray();
+            int[] orderedEvenNums = evenNums.OrderBy(identity).ToArray();
+
+            Console.WriteLine(String.Join(", ", orderedEvenNums));
         }
+
     }
 }
+
+
