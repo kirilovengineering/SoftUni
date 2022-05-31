7059d5c9d8784000ca31838d8e7655935e11417d
kirilovengineering <kirilovengineering@gmail.com>
2022-05-31 20:25:30 +0300

    ActionWhatIs

---
+++ C# Advanced/FunctionalProgramming-Lab/ActionWhatIs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ActionWhatIs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // функция, която не връща нищо
+            void Print(string s)
+            {
+                Console.WriteLine(s);
+            }
+
+            // Когато нещо в C# не връща стойност това е Екшън
+            Action<string> print = Print;
+
+            //после екшъна може да се извиква с параметър "Петър", или без параметър
+
+            print("Petar");
+            print(5.ToString());
+
+            // Mоже да е и с ламда експрешън
+            Action<string> printLambda =
+                message => Console.WriteLine(message);
+
+            printLambda("This is lambda message");
+            printLambda(5.ToString());
+
+
+        }
+    }
+}
