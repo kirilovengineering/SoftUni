300a52f0be6078991e1b1d938b4ba5291c6ae1e1
kirilovengineering <kirilovengineering@gmail.com>
2022-06-02 23:21:38 +0300

    100/100

--- C# Advanced/FunctionalProgramming-Exercise/05._Applied_Arithmetics/Program.cs
+++ C# Advanced/FunctionalProgramming-Exercise/05._Applied_Arithmetics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Applied_Arithmetics
 {
@@ -6,7 +8,39 @@ namespace _05._Applied_Arithmetics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+
+            Func<List<int>, List<int>> add = list => list.Select(number => number += 1).ToList();
+            Func<List<int>, List<int>> multiplay = list => list.Select(number => number *= 2).ToList();
+            Func<List<int>, List<int>> substract = list => list.Select(number => number -= 1).ToList();
+            Action<List<int>> print = list => Console.WriteLine(string.Join(" ", list));
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        //получваме списък и връщаме модифициран списък като всеки елемент е увеличен с 1
+                        numbers = add(numbers);
+                        break;
+                    case "multiply":
+                        //получваме списък и връщаме модифициран списък като всеки елемент е умножен с 2
+                        numbers = multiplay(numbers);
+                        break;
+                    case "subtract":
+                        //получваме списък и връщаме модифициран списък като всеки елемент е намален с 1
+                        numbers = substract(numbers);
+                        break;
+                    case "print":
+                        //получваме списък и го принтираме
+                        print(numbers);
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
         }
     }
 }
--- C# Advanced/FunctionalProgramming-Exercise/09._Predicate_Party!/Program.cs
+++ C# Advanced/FunctionalProgramming-Exercise/09._Predicate_Party!/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _09._Predicate_Party_
 {
@@ -6,7 +8,23 @@ namespace _09._Predicate_Party_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> people = Console.ReadLine().Split(" ").ToList();
+            string command = Console.ReadLine();
+
+            while (command != "Party!")
+            {
+                //командит може да са "Double ..." или "Remove ..."
+                if (command.StartsWith("Double"))
+                {
+                    //дублиране
+                }
+                else if (command.StartsWith("Remove"))
+                {
+                    //премахване
+                }
+
+                command = Console.ReadLine();
+            }
         }
     }
 }
