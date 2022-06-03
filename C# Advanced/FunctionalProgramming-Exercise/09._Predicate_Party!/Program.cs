062a8ea4de80df59ee621a451c33e76eff1d4800
kirilovengineering <kirilovengineering@gmail.com>
2022-06-03 17:09:46 +0300

    100/100

--- C# Advanced/FunctionalProgramming-Exercise/09._Predicate_Party!/Program.cs
+++ C# Advanced/FunctionalProgramming-Exercise/09._Predicate_Party!/Program.cs
@@ -8,23 +8,55 @@ namespace _09._Predicate_Party_
     {
         static void Main(string[] args)
         {
-            List<string> people = Console.ReadLine().Split(" ").ToList();
-            string command = Console.ReadLine();
-
-            while (command != "Party!")
+            List<string> people = Console.ReadLine().Split().ToList();
+            while (true)
             {
-                //командит може да са "Double ..." или "Remove ..."
-                if (command.StartsWith("Double"))
+                string command = Console.ReadLine();
+                if (command == "Party!")
                 {
-                    //дублиране
-                }
-                else if (command.StartsWith("Remove"))
-                {
-                    //премахване
+                    Console.WriteLine(people.Count != 0 ? $"{String.Join(", ", people)} are going to the party!" : "Nobody is going to the party!");
+                    break;
                 }
 
-                command = Console.ReadLine();
+                string[] tokens = command.Split();
+                command = tokens[0];
+                string criteria = tokens[1];
+                Predicate<string> filter;
+
+                switch (command)
+                {
+                    case "Remove":
+                        filter = CreateFilter(tokens, criteria);
+                        people.RemoveAll(filter);
+                        break;
+
+                    case "Double":
+                        filter = CreateFilter(tokens, criteria);
+                        people.AddRange(people.Select(x => x).Where(x => filter(x)).ToArray());
+                        break;
+
+                    default:
+                        break;
+                }
             }
+        }
+
+        public static Predicate<string> CreateFilter(string[] tokens, string criteria)
+        {
+            Predicate<string> filter;
+            switch (criteria)
+            {
+                case "StartsWith": filter = x => x.StartsWith(tokens[2]); break;
+                case "EndsWith": filter = x => x.EndsWith(tokens[2]); break;
+
+                case "Length":
+                    int length = int.Parse(tokens[2]);
+                    filter = x => x.Length == length; break;
+
+                default: filter = null; break;
+            }
+
+            return filter;
         }
     }
 }
