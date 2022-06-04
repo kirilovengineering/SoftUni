ac44e021f64d7747877b2ae28debdb311787b5b6
kirilovengineering <kirilovengineering@gmail.com>
2022-06-04 17:33:52 +0300

    Ще трябва нов проект по условията на заданието.

--- C# Advanced/Defining_Classes-Lab/01._Car/Program.cs
+++ C# Advanced/Defining_Classes-Lab/01._Car/Program.cs
@@ -2,11 +2,40 @@
 
 namespace _01._Car
 {
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        { 
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car ce = new Car();
+
+            ce.Make = "VW";
+            ce.Model = "MK3";
+            ce.Year = 1992;
+
+            Console.WriteLine($"Make: {ce.Make}\nMoldel: {ce.Model}\nYear: {ce.Year}");
         }
     }
 }
