53c8ebfbc28ab6809003815ef2bdf3af6d325df4
kirilovengineering <kirilovengineering@gmail.com>
2022-06-04 18:24:21 +0300

    0

--- C# Advanced/Defining_Classes-Lab/01._Car/CarManufacturer/CarManufacturer/Car.cs
+++ C# Advanced/Defining_Classes-Lab/01._Car/CarManufacturer/CarManufacturer/Car.cs
@@ -1,15 +1,13 @@
-﻿using System;
-
-namespace _01._Car
+﻿namespace CarManufacturer
 {
-    class Car
+    internal class Car
     {
         private string make;
         private string model;
         private int year;
 
         public string Make
-        { 
+        {
             get { return make; }
             set { make = value; }
         }
@@ -23,19 +21,6 @@ namespace _01._Car
         {
             get { return this.year; }
             set { this.year = value; }
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Car ce = new Car();
-
-            ce.Make = "VW";
-            ce.Model = "MK3";
-            ce.Year = 1992;
-
-            Console.WriteLine($"Make: {ce.Make}\nMoldel: {ce.Model}\nYear: {ce.Year}");
         }
     }
 }
---
+++ C# Advanced/Defining_Classes-Lab/01._Car/CarManufacturer/CarManufacturer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car ce = new Car();
+
+            ce.Make = "VW";
+            ce.Model = "MK3";
+            ce.Year = 1992;
+
+            Console.WriteLine($"Make: {ce.Make}\nMoldel: {ce.Model}\nYear: {ce.Year}");
+        }
+    }
+}
