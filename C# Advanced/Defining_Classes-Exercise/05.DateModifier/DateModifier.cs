112753d503d237a70c49ee3a39111d5662a15a24
kirilovengineering <kirilovengineering@gmail.com>
2022-06-05 18:41:56 +0300

    100/100

---
+++ C# Advanced/Defining_Classes-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        //-------------- Constructors --------------
+        public DateModifier(string date1, string date2)
+        {
+            this.Date1 = DateTime.ParseExact(date1, "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+            this.Date2 = DateTime.ParseExact(date2, "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        //--------------- Properties ---------------
+        public DateTime Date1 { get; private set; }
+
+        public DateTime Date2 { get; private set; }
+
+        //---------------- Methods -----------------
+        public int GetDaysDifference(string date1, string date2)
+        {
+            return Math.Abs((this.Date1.Date - this.Date2.Date).Days);
+        }
+    }
+}
---
+++ C# Advanced/Defining_Classes-Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(date1, date2);
+            Console.WriteLine(dateModifier.GetDaysDifference(date1, date2));
+        }
+    }
+}
