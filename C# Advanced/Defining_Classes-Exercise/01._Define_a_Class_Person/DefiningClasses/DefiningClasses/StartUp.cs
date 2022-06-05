ad37a4e559c128cf7405b16b7a6e6054788c24a3
kirilovengineering <kirilovengineering@gmail.com>
2022-06-05 13:32:36 +0300

    100/100

---
+++ C# Advanced/Defining_Classes-Exercise/01._Define_a_Class_Person/DefiningClasses/DefiningClasses/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person
+            {
+                Name = "Peter",
+                Age = 20
+            };
+
+            Person person2 = new Person();
+            person2.Name = "George";
+            person2.Age = 18;
+
+            Person person3 = new Person();
+            person2.Name = "Jose";
+            person2.Age = 34;
+
+        }
+    }
+}
