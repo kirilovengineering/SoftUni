f5bb715718ffdfe892b11cccbab8e0e57cc59304
kirilovengineering <kirilovengineering@gmail.com>
2022-06-07 22:49:56 +0300

    Implementing_Stack_and_Queue-Lab

--- C# Advanced/Implementing_Stack_and_Queue-Lab/CustomDataStructures/CustomDataStructures.cs
+++ C# Advanced/Implementing_Stack_and_Queue-Lab/CustomDataStructures/CustomDataStructures.cs
@@ -6,7 +6,53 @@ namespace CustomDataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Test Add Method
+            var list = new CustomList<int>();
+
+            list.Add(5);
+            Console.WriteLine($"Adding element {list[0]}");
+            list.Add(6);
+            Console.WriteLine($"Adding element {list[1]}");
+            list.Add(7);
+            Console.WriteLine($"Adding element {list[2]}");
+            list.Add(8);
+            Console.WriteLine($"Adding element {list[3]}");
+            Console.WriteLine($"List count are6 {list.Count}");
+            Console.WriteLine($"-------------" + Environment.NewLine + "------------------");
+            int removedElement = list.RemoveAt(2);
+            Console.WriteLine($"removedElement {removedElement} at index 2");
+            Console.WriteLine($"List count after removing element {list.Count}");
+
+            Console.WriteLine($"-------------" + Environment.NewLine + "------------------");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"Current element {list[i]}");
+            }
+            Console.WriteLine($"-------------" + Environment.NewLine + "------------------");
+            // Test InsetAt Method
+            var testInsert = new CustomList<int>();
+
+            testInsert.Add(5);
+            Console.WriteLine($"Adding element {testInsert[0]}");
+            testInsert.Add(6);
+            Console.WriteLine($"Adding element {testInsert[1]}");
+            testInsert.Add(7);
+            Console.WriteLine($"Adding element {testInsert[2]}");
+            testInsert.Add(8);
+            Console.WriteLine($"Adding element {testInsert[3]}");
+            testInsert.InsertAt(1,23);
+
+            Console.WriteLine($"Inserted elelment in index 1 is {testInsert[1]}");
+
+            for (int i = 0; i < testInsert.Count; i++)
+            {
+                Console.WriteLine($"Current element {testInsert[i]}");
+            };
+
+            //......tests
+
         }
     }
 }
