5342e037274b3caf5008b2d1fdc2737dd8c29191
kirilovengineering <kirilovengineering@gmail.com>
2022-06-07 18:40:46 +0300

    Set Workshop

---
+++ C# Advanced/Implementing_Stack_and_Queue-Lab/Implementing_Stack_and_Queue-Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Implementing_Stack_and_Queue_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
