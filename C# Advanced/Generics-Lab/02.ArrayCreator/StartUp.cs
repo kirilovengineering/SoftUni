402bbe472b76a95792d67904d3bdf1b3bf391f79
kirilovengineering <kirilovengineering@gmail.com>
2022-06-08 20:36:40 +0300

    Rest Eqality Scale

--- C# Advanced/Generics-Lab/02.ArrayCreator/StartUp.cs
+++ C# Advanced/Generics-Lab/02.ArrayCreator/StartUp.cs
@@ -8,6 +8,14 @@ namespace _02.ArrayCreator
         {
             string[] strings = ArrayCreator.Create(5, "Pesho");
             int[] integers = ArrayCreator.Create(10, 33);
+
+            for (int i = 0; i < strings.Length; i++)
+                Console.WriteLine(strings[i]);
+
+            for (int i = 0; i < integers.Length; i++)
+                Console.WriteLine(integers[i]);
+
+
         }
     }
 }
--- C# Advanced/Generics-Lab/03.GenericScale/StartUp.cs
+++ C# Advanced/Generics-Lab/03.GenericScale/StartUp.cs
@@ -6,6 +6,18 @@ namespace _03.GenericScale
     {
         static void Main(string[] args)
         {
+            EqualityScale<int> cupleInt = new EqualityScale<int>(1, 1);
+
+            bool eqInt = cupleInt.AreEqual();
+            Console.WriteLine(eqInt);
+            // Връща true при сравнениен на двете инт съойности
+
+
+            EqualityScale<String> cupleString = new EqualityScale<String>("Kiril", "NotKiril");
+
+            bool eqString = cupleString.AreEqual();
+            Console.WriteLine(eqString);
+            // Връща false при сравнение на двата стринга
 
         }
     }
