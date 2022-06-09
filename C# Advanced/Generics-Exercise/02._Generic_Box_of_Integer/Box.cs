173306842933e0ed6efc4f6b566d44f55f77494d
kirilovengineering <kirilovengineering@gmail.com>
2022-06-09 23:15:21 +0300

    100/100

--- C# Advanced/Generics-Exercise/02._Generic_Box_of_Integer/Box.cs
+++ C# Advanced/Generics-Exercise/02._Generic_Box_of_Integer/Box.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _01._Generic_Box_of_String
+namespace _02._Generic_Box_of_Integer
 {
+
     // Какво се случва
     public class Box<T> // 1.като кажем че входящия тип е инт тогава 
     {
+        // ----------------------- Fields -----------------------
+        private T element;
+        // -------------------- Constructors --------------------
         public Box(T element) // 2.element става/сетва се от тип инт
         {
             this.Element = element; // <- element става инт; 3. Element става Инт 
         }
-
+        //--------------------- Properties ---------------------
         public T Element { get; private set; } //4.  Element от инт става публичен  и може да се гетне, ....сетва се само вътрешно в класа
 
         // при public T Element { get; private set; } метода в класа може да промена Element
         // public T Element { get; } при този запис никой не може да променя Element
 
-
+        //---------------------- Methods -----------------------
         //5. Разпечатваме на екрана с името на типа и самия елемент
         public override string ToString()
         {
---
+++ C# Advanced/Generics-Exercise/02._Generic_Box_of_Integer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._Generic_Box_of_Integer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
