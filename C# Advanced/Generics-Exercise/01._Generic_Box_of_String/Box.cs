08f32a0e1981652b66638a293254d7a94befabf5
kirilovengineering <kirilovengineering@gmail.com>
2022-06-09 22:10:22 +0300

    Generic_Box_of_String

---
+++ C# Advanced/Generics-Exercise/01._Generic_Box_of_String/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Generic_Box_of_String
+{
+    // Какво се случва
+    public class Box<T> // 1.като кажем че входящия тип е инт тогава 
+    {
+        public Box(T element) // 2.element става/сетва се от тип инт
+        {
+            this.Element = element; // <- element става инт; 3. Element става Инт 
+        }
+
+        public T Element { get; private set; } //4.  Element от инт става публичен  и може да се гетне, сетва се само вътрешно в класа
+
+        //5. Разпечатваме на екрана с името на типа и самия елемент
+        public override string ToString()
+        {
+            return $"{this.Element.GetType().FullName}: {this.Element}";
+            // Друг оувъррайд
+            // return $"{typeof(T)}: {this.Element}";
+        }
+
+    }
+}
---
+++ C# Advanced/Generics-Exercise/01._Generic_Box_of_String/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Generic_Box_of_String
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
