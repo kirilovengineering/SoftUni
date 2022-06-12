abfb71eec0cd072a6d834195caea9bce372c4c5f
kirilovengineering <kirilovengineering@gmail.com>
2022-06-12 17:28:37 +0300

    Generics

---
+++ C# Advanced/Generics-Exercise/06._Generic_Count_Method_Double/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._Generic_Count_Method_Double
+{
+    public class Box<T> : IComparable<T>
+        where T : IComparable<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public int CompareTo(T other)
+        {
+            return this.Value.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+}
