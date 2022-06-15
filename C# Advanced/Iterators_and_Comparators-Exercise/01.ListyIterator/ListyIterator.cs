5fbd7acbde01f77bc9e5dac139a14acee352da45
kirilovengineering <kirilovengineering@gmail.com>
2022-06-15 23:30:57 +0300

    Exercise: Iterators and Comparators

---
+++ C# Advanced/Iterators_and_Comparators-Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        //---------------- Fields ------------------
+        private List<T> collection;
+        private int currentIndex;
+
+        //------------- Constructors ---------------
+        public ListyIterator(params T[] collection)
+        {
+            this.collection = new List<T>(collection);
+        }
+
+        //--------------- Methods ------------------
+        public bool Move()
+        {
+            if (this.collection.Count - 1 > currentIndex)
+            {
+                currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex + 1 < collection.Count;
+        }
+
+        public void Print()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.collection[currentIndex]);
+        }
+    }
+}
---
+++ C# Advanced/Iterators_and_Comparators-Exercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split();
+                switch (tokens[0])
+                {
+                    case "Create":
+                        listyIterator = new ListyIterator<string>(tokens.Skip(1).ToArray());
+                        break;
+
+                    case "Print":
+                        try
+                        {
+                            listyIterator.Print();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
