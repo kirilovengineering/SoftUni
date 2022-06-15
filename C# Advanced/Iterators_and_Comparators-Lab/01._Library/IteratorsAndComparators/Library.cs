65e92cecd7b7cec10451ed9c3d9fa356e5df6bf7
kirilovengineering <kirilovengineering@gmail.com>
2022-06-15 22:17:38 +0300

    IteratorsAndComparators

--- C# Advanced/Iterators_and_Comparators-Lab/01._Library/IteratorsAndComparators/Library.cs
+++ C# Advanced/Iterators_and_Comparators-Lab/01._Library/IteratorsAndComparators/Library.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace IteratorsAndComparators
 {
-    internal class Library
+    public class Library : IEnumerable<Book>
     {
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; private set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            for (int i = 0; i < this.Books.Count; i++)
+            {
+                yield return this.Books[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
     }
 }
